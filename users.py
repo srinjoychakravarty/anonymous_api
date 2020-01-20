3414fb1a59615643d90d402fa44b0e60d7fd759d
Srinjoy Chakravarty <sc7839@nyu.edu>
2020-01-20 20:50:20 +0300

    updated users to use database and sqlalchemy :heavy_plus_sign:

--- users.py
+++ users.py
@@ -1,4 +1,4 @@
-user"""
+"""
 This is the users module and supports all the REST actions for the
 USERS collection
 """
@@ -31,74 +31,85 @@ USERS = {                                                                       
 def read_all():
     """
     This function responds to a request for /v1/users
-    with the complete lists of users
+    with the complete list of users
     :return:        json string of list of users
     """
     users = User.query.order_by(User.lname).all()                               # Create the list of users from our data
     user_schema = UserSchema(many = True)                                       # Serialize the data for the response
-    return user_schema.dump(users).data
+    serialized_data = user_schema.dump(users).data
+    return serialized_data
 
 def read_one(user_id):
     """
     This function responds to a request for /v1/users/{user_id}
     with one matching user from users
-    :param user_id:   ID of user to find
-    :return:            user matching ID
+    :param user_id:     id of user to find
+    :return:            user matching id
     """
     user = User.query.filter(User.user_id == user_id).one_or_none()             # Get the user requested
     if user is not None:                                                        # Did we find a user?
         user_schema = UserSchema()                                              # Serialize the data for the response
-        return user_schema.dump(user).data                                      # Otherwise, nope, didn't find that user
-    else:
+        serialized_data = user_schema.dump(user).data
+        return serialized_data
+    else:                                                                       # Otherwise, nope, didn't find that user
         abort(404, 'User not found for Id: {user_id}'.format(user_id = user_id))
 
 def create(user):
     """
-    This function creates a new user in the people structure
+    This function creates a new user in the users structure
     based on the passed-in user data
-    :param user:  user to create in people structure
-    :return:        201 on success, 406 on user exists
+    :param user:    user to create in users structure
+    :return:        201 on success, 409 on user exists
     """
     fname = user.get('fname')
     lname = user.get('lname')
     existing_user = User.query.filter(User.fname == fname).filter(User.lname == lname).one_or_none()
     if existing_user is None:                                                   # Can we insert this user?
         schema = UserSchema()                                                   # Create a user instance using the schema and the passed-in user
-        new_user = schema.load(user, session=db.session).data
-        db.session.add(new_user)                                                # Add the user to the database
-        db.session.commit()
-        return schema.dump(new_user).data, 201                                  # Serialize and return the newly created user in the response
+        new_user = schema.load(user, session = sql_alchemy_db.session).data
+        sql_alchemy_db.session.add(new_user)                                    # Add the user to the database
+        sql_alchemy_db.session.commit()
+        serialized_data = schema.dump(new_user).data                            # Serialize and return the newly created user in the response
+        return serialized_data, 201
     else:                                                                       # Otherwise, nope, user exists already
-        abort(409, f'User {fname} {lname} exists already')
+        abort(409, "User {firstname} {lastname} exists already".format(firstname = fname, lastname = lname))
 
-def update(lname, user):
+def update(user_id, user):
     """
     This function updates an existing user in the users structure
-    :param lname:   last name of user to update in the users structure
-    :param user:  user to update
-    :return:        updated user structure
+    Throws an error if a user with the name we want to update to
+    already exists in the database.
+    :param user_id:   id of the user to update in the users structure
+    :param user:      user to update
+    :return:          updated user structure
     """
-    if lname in USERS:                                                          # Does the user exist in users?
-        USERS[lname]["fname"] = user.get("fname")
-        USERS[lname]["timestamp"] = get_timestamp()
-        return USERS[lname]
-    else:                                                                       # otherwise, nope, that's an error
-        abort(
-            404, "User with last name {lname} not found".format(lname=lname)
-        )
+    update_user = User.query.filter(User.user_id == user_id).one_or_none()      # Get the user requested from the db into session
+    fname = user.get("fname")                                                   # Try to find an existing user with the same name as the update
+    lname = user.get("lname")
+    existing_user = (User.query.filter(User.fname == fname).filter(User.lname == lname).one_or_none())
+    if update_user is None:                                                     # Are we trying to find a user that does not exist?
+        abort(404, "User not found for id: {user}".format(user = user_id))
+    elif (existing_user is not None and existing_user.user_id != user_id):      # Would our update create a duplicate of another user already existing?
+        abort(409, "User {firstname} {lastname} exists already".format(firstname = fname, lastname = lname))
+    else:                                                                       # Otherwise go ahead and update!
+        schema = UserSchema()                                                   # turn the passed in user into a db object
+        update = schema.load(user, session = sql_alchemy_db.session).data
+        update.user_id = update_user.user_id                                    # Set the id to the user we want to update
+        sql_alchemy_db.session.merge(update)                                    # merge the new object into the old and commit it to the db
+        sql_alchemy_db.session.commit()
+        serialized_data = schema.dump(update_user).data                         # return updated user in the response
+        return serialized_data, 200
 
-def delete(lname):
+def delete(user_id):
     """
     This function deletes a user from the users structure
-    :param lname:   last name of user to delete
-    :return:        200 on successful delete, 404 if not found
+    :param user_id:     id of the user to delete
+    :return:            200 on successful delete, 404 if not found
     """
-    if lname in USERS:                                                          # Does the user to delete exist?
-        del USERS[lname]
-        return make_response(
-            "{lname} successfully deleted".format(lname=lname), 200
-        )
-    else:                                                                       # Otherwise, nope, user to delete not found
-        abort(
-            404, "User with last name {lname} not found".format(lname=lname)
-        )
+    user = User.query.filter(User.user_id == user_id).one_or_none()             # Get the user requested
+    if user is not None:                                                        # Did we find a user?
+        sql_alchemy_db.session.delete(user)
+        sql_alchemy_db.session.commit()
+        return make_response("User {user} deleted".format(user = user_id), 200)
+    else:                                                                       # Otherwise, nope, didn't find that user
+        abort(404, "User not found for id: {user}".format(user = user_id))
