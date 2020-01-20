f728b9002a0c877fcf618488ab906697e92a5683
Srinjoy Chakravarty <sc7839@nyu.edu>
2020-01-20 21:55:00 +0300

    minor updates to core server :package:

--- flask_micro_server.py
+++ flask_micro_server.py
@@ -1,10 +1,14 @@
-import connexion
+"""
+Main module of the server file
+"""
+# local modules
+import config
 
-# Create the application instance
-app = connexion.App(__name__, specification_dir = './')
+# Get the application instance
+connex_app = config.connexion_application
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.yml')
+connex_app.add_api('swagger.yml')
 
 # Create a URL route in our application for "/"
 @app.route('/')
@@ -15,4 +19,4 @@ def home():
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000, debug = True)
+    connex_app.run(debug = True)
