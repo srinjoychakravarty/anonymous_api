133c99b796c4012d4c496a2e77beca383755a885
Srinjoy Chakravarty <sc7839@nyu.edu>
2020-01-20 22:13:56 +0300

    changed deletion criteria of seeing script :mag:

--- config.py
+++ config.py
@@ -4,21 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Create the Connexion application instance
-connexion_application = connexion.App(__name__, specification_dir = basedir)
-
-# Get the underlying Flask app instance
-flask_application = connexion_application.app
-
-# Configure the SQLAlchemy part of the app instance
-flask_application.config['SQLALCHEMY_ECHO'] = True
-flask_application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'people.db')
+connexion_application = connexion.App(__name__, specification_dir = basedir)    # Create the Connexion application instance
+flask_application = connexion_application.app                                   # Get the underlying Flask app instance
+flask_application.config['SQLALCHEMY_ECHO'] = True                              # Configure the SQLAlchemy part of the app instance
+flask_application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'users.db')
 # flask_application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@127.0.0.1/postgres'
 flask_application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Create the SQLAlchemy db instance
-sql_alchemy_db = SQLAlchemy(flask_application)
-
-# Initialize Marshmallow
-marshmallow = Marshmallow(flask_application)
+sql_alchemy_db = SQLAlchemy(flask_application)                                  # Create the SQLAlchemy db instance
+marshmallow = Marshmallow(flask_application)                                    # Initialize Marshmallow
--- seed_database.py
+++ seed_database.py
@@ -1,23 +1,16 @@
-USERimport os
+import os
 from config import sql_alchemy_db
 from models import User
 
-# Data to initialize database with
-USERS = [
+USERS = [                                                                       # Data to seed database with
     {'fname': 'Doug', 'lname': 'Farrell'},
     {'fname': 'Kent', 'lname': 'Brockman'},
     {'fname': 'Bunny','lname': 'Easter'}
 ]
-
-# Delete database file if it exists currently
-if os.path.exists('users.db'):
+if (os.path.exists('users.db')):                                                # Delete database file if it exists currently
     os.remove('users.db')
-
-# Create the database
-sql_alchemy_db.create_all()
-
-# Iterate over the USERS structure and populate the database
-for user in USERS:
-    p = User(lname=user['lname'], fname=user['fname'])
-    sql_alchemy_db.session.add(p)
+sql_alchemy_db.create_all()                                                     # Create the database
+for user in USERS:                                                              # Iterate over the USERS structure and populate the database
+    usr = User(lname=user['lname'], fname=user['fname'])
+    sql_alchemy_db.session.add(usr)
 sql_alchemy_db.session.commit()
