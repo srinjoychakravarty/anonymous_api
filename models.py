f3bd7d2043db21d649386b8962952f98806ac8ab
Srinjoy Chakravarty <sc7839@nyu.edu>
2020-01-20 22:04:43 +0300

    removed indexing on last name :mute:

--- models.py
+++ models.py
@@ -3,10 +3,10 @@ from config import sql_alchemy_db, marshmallow
 
 class User(sql_alchemy_db.Model):
     __tablename__ = 'user'
-    user_id = sql_alchemy_db.Column(sql_alchemy_db.Integer, primary_key=True)
-    lname = sql_alchemy_db.Column(sql_alchemy_db.String(32), index=True)
+    user_id = sql_alchemy_db.Column(sql_alchemy_db.Integer, primary_key = True)
+    lname = sql_alchemy_db.Column(sql_alchemy_db.String(32))
     fname = sql_alchemy_db.Column(sql_alchemy_db.String(32))
-    timestamp = sql_alchemy_db.Column(sql_alchemy_db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    timestamp = sql_alchemy_db.Column(sql_alchemy_db.DateTime, default = datetime.utcnow, onupdate = datetime.utcnow)
 
 class UserSchema(marshmallow.ModelSchema):
     class Meta:
